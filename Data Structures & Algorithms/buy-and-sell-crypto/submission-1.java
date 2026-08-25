18bb8f9f5d6e0c6cb07cc6ffa4ce1ad644e8a258
Ane Fernandes <56165975+anefernandes@users.noreply.github.com>
2026-08-25 16:32:55 -0300

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int buy = 0;
+       int sell = 1;
+       int maxP = 0;
+
+       while(sell < prices.length){
+            if(prices[sell] > prices[buy]){
+                maxP = Math.max(maxP, prices[sell] - prices[buy]);
+            }
+            else {
+                buy = sell;
+            }
+            sell++;
+       }
+       return maxP;
+    }
+}
